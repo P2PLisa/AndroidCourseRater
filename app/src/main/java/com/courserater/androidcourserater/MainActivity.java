4c94a73e4a70419fddef8b4ae825339ecaf253cc
Sophia Chen <chen1800@purdue.edu>
2016-01-17 01:33:37 -0500

    Splash Screen by Sophia

---
+++ app/src/main/java/com/courserater/androidcourserater/MainActivity.java
@@ -0,0 +1,22 @@
+package com.courserater.androidcourserater;
+
+/**
+ * Created by chenzhuoya on 1/16/16.
+ */
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+}
